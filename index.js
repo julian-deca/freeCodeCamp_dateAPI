d117d7a92a23ee8ec4fc9fc5ac2f78f8513cc24f
julian <juliandecastelli@gmail.com>
2022-08-01 17:40:07 -0300

    change date model to UTC

--- index.js
+++ index.js
@@ -4,9 +4,6 @@
 // init project
 var express = require("express");
 var app = express();
-let a = new Date(2015, 11, 25, 0, 0, 0);
-console.log(a);
-console.log(a.getTime());
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -23,47 +20,19 @@ app.get("/", function (req, res) {
 
 // your first API endpoint...
 app.get("/api/:date", function (req, res) {
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
   const myRegex = /[-]/g;
   let time;
   let data;
   myRegex.test(req.params.date)
     ? ((time = req.params.date.split("-")),
-      (data = new Date(Number(time[0]), Number(time[1]) - 1, Number(time[2]))))
-    : (data = new Date(Number(req.params.date)));
-
-  const day = days[data.getUTCDay()];
-  const date = data.getUTCDate();
-  const month = months[data.getUTCMonth()];
-  const year = data.getUTCFullYear();
-  const hour =
-    data.getUTCHours() < 10 ? "0" + data.getUTCHours() : data.getUTCHours();
-  const minutes =
-    data.getUTCMinutes() < 10
-      ? "0" + data.getUTCMinutes()
-      : data.getUTCMinutes();
-  const seconds =
-    data.getUTCSeconds() < 10
-      ? "0" + data.getUTCSeconds()
-      : data.getUTCSeconds();
+      (data = new Date(
+        Date.UTC(Number(time[0]), Number(time[1]) - 1, Number(time[2]))
+      )))
+    : (data = new Date(Date.UTC(Number(req.params.date))));
 
   res.json({
     unix: data.getTime(),
-    utc: `${day}, ${date} ${month} ${year} ${hour}:${minutes}:${seconds} GMT`,
+    utc: data.toUTCString(),
   });
 });
 
